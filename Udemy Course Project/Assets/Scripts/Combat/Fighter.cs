e23637806b4949bc5810da78eed09fbd5d6c359b
HaroldHermoza <56950303+HaroldHermoza@users.noreply.github.com>
2022-06-27 11:16:04 +0800

    Halfway Section 5
    Halfway Section 5

---
+++ Udemy Course Project/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
+
+        Health target;
+        float timeSinceLastAttack = Mathf.Infinity;
+
+        private void Update()
+        {
+            timeSinceLastAttack += Time.deltaTime;
+
+            if (target == null) return;
+            if (target.IsDead()) return;
+            if (target != null && !GetIsInRange())
+            {
+                GetComponent<PlayerMover>().MoveTo(target.transform.position);
+            }
+
+            else
+            {
+                GetComponent<PlayerMover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            transform.LookAt(target.transform);
+
+            if(timeSinceLastAttack > timeBetweenAttacks)
+            {
+                TriggerAttack();
+                timeSinceLastAttack = 0;
+                //Trigger hit() event.
+            }
+            
+        }
+
+        private void TriggerAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("stopAttack");
+            GetComponent<Animator>().SetTrigger("attack");
+        }
+        //Animation Event
+        void Hit()
+        {
+            if(target == null) { return; }
+            target.TakeDamage(weaponDamage);
+        }
+
+        public void Attack(GameObject combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.GetComponent<Health>();
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+        }
+
+        public bool CanAttack(GameObject combatTarget)
+        {
+            if (combatTarget == null) { return false; }
+            Health targetToTest = combatTarget.GetComponent<Health>();
+            return targetToTest != null && !targetToTest.IsDead();
+        }
+        public void Cancel()
+        {
+            StopAttack();
+            target = null;
+        }
+
+        private void StopAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("attack");
+            GetComponent<Animator>().SetTrigger("stopAttack");
+        }
+
+
+    }
+
+}
